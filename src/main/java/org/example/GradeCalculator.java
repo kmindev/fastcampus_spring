d441090afb71d4914bd63df4d80bda50b0c138bd
KyeongMin Kang <kangkm0324@gmail.com>
2022-09-24 16:09:44 +0900

    Initial commit

---
+++ src/main/java/org/example/GradeCalculator.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.List;
+
+
+public class GradeCalculator {
+    private final Courses courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = new Courses(courses);
+    }
+
+    /**
+     * 요구사항
+     * 평균학점 계산 방법 = (학점수*교과목 평점)의 합계/수강신청 총학점 수
+     * 일급 컬렉션 사용
+     */
+
+
+    /**
+     * 핵심 포인트
+     */
+    //이수한 과목을 전달하여 평균학점 계산 요청 ---> 학점 계산기 ---> (학점수*교과목 평점)의 합계  ---> 과목(코스) 일급 컬렉션
+    //                                                   ---> 수강신청 총학점 수         ---> 과목(코스) 일급 컬렉션
+    public double calculateGrade() {
+        // (학점수*교과목 평점)의 합계
+        double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
+        // 수강신청 총학점 수
+        int totalCompletedCredit = courses.calculateTotalCompletedCredit();
+
+        return totalMultipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+}
